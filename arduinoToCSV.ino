9c02a5f7f65f7c5e30e4a160b11cbb9ca75d0895
TThetier <t.thetier@gmail.com>
2020-03-19 10:46:10 +0100

    Arduino code for the writing to CSV python script
    Modified arduino code to go with the python script that reads the serial port and write the data in a CSV file

---
+++ arduinoToCSV.ino
@@ -0,0 +1,29 @@
+int P = 0;
+int V = 0;
+int Tr = 0;
+int Pset = 12;
+int Vset = 20;
+int BPMset = 8;
+int Trset = 15;
+int cnt = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+
+  if(cnt == 20) 
+  {cnt=0;Tr=!Tr;}
+  else cnt+=1;
+  Serial.print(P);
+  Serial.print(",");
+  Serial.print(V);
+  Serial.print(",");
+  Serial.println(Tr);
+  P += 1;
+  V += 2;
+  delay(100);
+}
