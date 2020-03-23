74cf298424a006609e3d21d6a1698d3d36487257
jantje <eclipse@baeyens.it>
2020-03-23 17:16:05 +0100

    Enable simply copy the code to work for both the motor and controller

---
+++ CoronaAdemhaling/BeademingRegullus/Beademinglus.ino
@@ -0,0 +1,19 @@
+/*
+ * Beademinglus.ino
+ *
+ *  Created on: 23 mrt. 2020
+ *      Author: jan
+ */
+#ifndef  __IN_ECLIPSE__
+
+void beademing_loop();
+void beademing_setup();
+void setup() {
+    Serial.begin(115200);
+    beademing_setup();
+}
+
+void beademing_loop() {
+    beademing_loop();
+}
+#endif
