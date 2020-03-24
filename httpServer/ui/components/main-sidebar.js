616e6babbaec4859e9717ae6681c09051272411a
Thomas Mattheussen <thomas.mattheussen@gmail.com>
2020-03-24 16:30:27 +0100

    Polishing frontend interface

--- httpServer/ui/components/main-sidebar.js
+++ httpServer/ui/components/main-sidebar.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import cx from 'classnames';
 import Link from 'next/link';
+import GaugeIcon from './icons/gauge';
+import TerminalIcon from './icons/terminal';
+import GearIcon from './icons/gear';
+import PersonIcon from './icons/person';
+import { useRouter } from 'next/router';
 
 const MainSidebar = ({ className, ...other }) => {
+    const { pathname: currentPath } = useRouter();
+
+    const menuItems = [
+        { path: '/', label: 'Dashboard', icon: <GaugeIcon /> },
+        { path: '/machines-profile', label: 'Machines profile', icon: <PersonIcon /> },
+        { path: '/thresholds', label: 'Thresholds', icon: <GearIcon /> },
+        { path: '/login', label: 'Log in', icon: <PersonIcon /> },
+        { path: '/logs', label: 'System logs', icon: <TerminalIcon /> },
+    ];
+
     return (
         <nav className={ cx('main-sidebar', className) } { ...other }>
             Logo
 
             <ul className="main-sidebar__menu">
-                <li><Link href="/"><a className="main-sidebar__link">Dashboard</a></Link></li>
-                <li><Link href="/machines-profile"><a className="main-sidebar__link">Machines profile</a></Link></li>
-                <li><Link href="/thresholds"><a className="main-sidebar__link">Thresholds</a></Link></li>
-                <li><Link href="/login"><a className="main-sidebar__link">Log in</a></Link></li>
-                <li><Link href="/logs"><a className="main-sidebar__link">System logs</a></Link></li>
+                {
+                    menuItems.map(({ path, label, icon }, index) => {
+                        return (
+                            <li className={ cx('main-sidebar__menu-item', { 'is-active': currentPath === path }) } key={ index }>
+                                <Link href={ path }>
+                                    <a className="main-sidebar__link">
+                                        { icon } <span className="u-d-none-xs u-d-inline-lg">{ label }</span>
+                                    </a>
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
             </ul>
         </nav>
     );
