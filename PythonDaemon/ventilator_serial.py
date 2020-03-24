ced933a1d68bd4fe8f22246a747058321139b97c
Frank Vanbever <frank.vanbever@essensium.com>
2020-03-24 10:50:17 +0100

    Add exception handling to serial decode
    At startup it's possible that we receive incomplete serial data. We need to
    account for this. We'll need to determine what level of errors are tolerable
    here. For testing purposes now I would stop the application so we get immediate
    feedback if this occurs more than once.
    
    Signed-off-by: Frank Vanbever <frank.vanbever@essensium.com>

--- PythonDaemon/ventilator_serial.py
+++ PythonDaemon/ventilator_serial.py
@@ -9,12 +9,27 @@ class SerialHandler():
     def __init__(self, db_queue, port='/dev/ttyACM0', baudrate=115200):
         self.ser = serial.Serial(port, baudrate)
         self.queue = db_queue
+        self.errorcounter = 0
 
     def run(self, name):
         print("Starting {}".format(name))
         while True:
             line = self.ser.readline()
-            line = line.decode('utf-8')
+            try:
+                line = line.decode('utf-8')
+            except UnicodeDecodeError:
+                print("Failure decoding serial message, continuing")
+                if self.errorcounter == 0:
+                    self.errorcounter += 1
+                    print("utf-8 decode errorcounter: {}".format(self.errorcounter))
+                    continue
+                else:
+                    print("Repeatedly unable to decode serial messages, aborting!")
+                    raise SystemExit(-1)
+                # TODO: At the start it can happen that we get an incorrect message
+                # due to incomplete data. I do a hard abort here to ensure that this only
+                # happens once. We need to determine what a tolerable level of failure is here.
+
             tokens = line.split('=', 1)
             val = tokens[-1].rstrip('\r\n')
             if line.startswith(('bpm=')):
