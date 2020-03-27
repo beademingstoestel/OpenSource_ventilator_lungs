29e64f1b58106488a42ed8c14db25581127fa523
Thibaut Vandervelden <thvdveld@vub.be>
2020-03-27 16:11:25 +0100

    Update single value display to allow other values.

--- httpServer/ui/components/single-value-display.js
+++ httpServer/ui/components/single-value-display.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import cx from 'classnames';
 
-const SingleValueDisplay = ({ value, name, status = 'default', className, ...other }) => {
+const SmallSingleValueDisplay = ({ name, value }) => {
+    return (<div>
+        <div className="single-value-display__name">{name}</div>
+        <div className="single-value-display__value-small">{value}</div>
+    </div>);
+};
+
+const SingleValueDisplay = ({
+    value,
+    name,
+    status = 'default',
+    children,
+    className,
+    ...other
+}) => {
     return (
-        <div className={ cx('single-value-display', `single-value-display--${ status }`, className) } { ...other }>
-            <div className="single-value-display__name">{ name }</div>
-            <div className="single-value-display__value">{ value.toFixed(2) }</div>
+        <div className={cx('single-value-display', `single-value-display--${status}`, className)} {...other}>
+            <div className={cx('single-value-display-big-column')}>
+                <div className="single-value-display__name">{name}</div>
+                <div className="single-value-display__value">{value.toFixed(2)}</div>
+            </div>
+            <div>
+                {children}
+            </div>
         </div>
     );
 };
 
-export default SingleValueDisplay;
+export { SingleValueDisplay, SmallSingleValueDisplay };
