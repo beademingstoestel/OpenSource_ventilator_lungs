1190521e7ba91556faea9c77e46630deaa6cfc67
Thomas Mattheussen <thomas.mattheussen@gmail.com>
2020-03-29 14:27:00 +0200

    remove externally served dependencies

--- httpServer/ui/pages/_app.js
+++ httpServer/ui/pages/_app.js
@@ -1,5 +1,4 @@
 import 'regenerator-runtime/runtime';
-import Head from 'next/head';
 import '../scss/index.scss';
 
 import { toast } from 'react-toastify';
@@ -10,10 +9,6 @@ toast.configure();
 const CustomApp = ({ Component, pageProps }) => {
     return (
         <>
-            <Head>
-                <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet" />
-                <script src="https://code.iconify.design/1/1.0.4/iconify.min.js"></script>
-            </Head>
             <Component {...pageProps} />
         </>
     );
