893b6cf252ff4990e21e4824d4387b1303757bac
Tom Bruyneel <tom.bruyneel@trisco.be>
2020-04-01 11:37:57 +0200

    add controller to save network settings

---
+++ httpServer/src/Controllers/NetworkSettingsController.ts
@@ -0,0 +1,30 @@
+// eslint-disable-next-line no-unused-vars
+import { Request, ResponseToolkit, HandlerDecorations, Lifecycle } from '@hapi/hapi';
+// eslint-disable-next-line no-unused-vars
+import { ISettingsRepository } from '../Repositories/ISettingsRepository';
+
+export class NetworkSettingsController {
+    constructor(private settingsRepository: ISettingsRepository) {}
+
+    async HandleGet(request: Request, h: ResponseToolkit) {
+        const networkSettings = await this.settingsRepository.GetSettings('network-settings');
+        const retValue = {
+            firstName: '',
+            lastName: '',
+            admittanceDate: new Date(),
+            info: '',
+        };
+
+        return { ...retValue, ...networkSettings };
+    }
+
+    async HandlePut(request: Request, h: ResponseToolkit) {
+        const settings = request.payload;
+
+        await this.settingsRepository.SaveSettings('network-settings', settings);
+
+        return {
+            result: true,
+        };
+    }
+};
