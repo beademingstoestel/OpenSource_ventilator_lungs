b2dc49a8b7f3e1af4a4fde5440f5ae567cc05586
Tom Bruyneel <tom.bruyneel@trisco.be>
2020-04-05 13:15:54 +0200

    use descending sort mode to index timestamps

--- httpServer/docker/db/mongo-init.js
+++ httpServer/docker/db/mongo-init.js
@@ -9,11 +9,11 @@ printjson(db.getSiblingDB("beademing").createCollection("settings"));
 printjson(db.getSiblingDB("beademing").createCollection("alarms"));
 printjson(db.getSiblingDB("beademing").createCollection("logs"));
 
-printjson(db.getSiblingDB("beademing").volume_values.createIndex( { loggedAt: 1 } ));
-printjson(db.getSiblingDB("beademing").pressure_values.createIndex( { loggedAt: 1 } ));
-printjson(db.getSiblingDB("beademing").breathsperminute_values.createIndex( { loggedAt: 1 } ));
-printjson(db.getSiblingDB("beademing").trigger_values.createIndex( { loggedAt: 1 } ));
-printjson(db.getSiblingDB("beademing").flow_values.createIndex( { loggedAt: 1 } ));
-printjson(db.getSiblingDB("beademing").cpu_values.createIndex( { loggedAt: 1 } ));
-printjson(db.getSiblingDB("beademing").logs.createIndex( { loggedAt: 1 } ));
+printjson(db.getSiblingDB("beademing").volume_values.createIndex( { loggedAt: -1 } ));
+printjson(db.getSiblingDB("beademing").pressure_values.createIndex( { loggedAt: -1 } ));
+printjson(db.getSiblingDB("beademing").breathsperminute_values.createIndex( { loggedAt: -1 } ));
+printjson(db.getSiblingDB("beademing").trigger_values.createIndex( { loggedAt: -1 } ));
+printjson(db.getSiblingDB("beademing").flow_values.createIndex( { loggedAt: -1 } ));
+printjson(db.getSiblingDB("beademing").cpu_values.createIndex( { loggedAt: -1 } ));
+printjson(db.getSiblingDB("beademing").logs.createIndex( { loggedAt: -1 } ));
 printjson(db.getSiblingDB("beademing").logs.createIndex( { severity: 1 } ));
